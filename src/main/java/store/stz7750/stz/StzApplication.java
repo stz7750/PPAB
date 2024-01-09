7094e5a7dea9de4f869ef1f7264df28d72a4ce84
stz7750 <rlaxogns7750@gmail.com>
2024-01-09 16:23:41 +0900

    first commit!

---
+++ src/main/java/store/stz7750/stz/StzApplication.java
@@ -0,0 +1,13 @@
+package store.stz7750.stz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/store/stz7750/stz/StzApplicationTests.java
@@ -0,0 +1,13 @@
+package store.stz7750.stz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
